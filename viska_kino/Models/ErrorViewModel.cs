6efa3b019644670658033a1159522cbfca4eecd9
Teodor Balcar <teodorbalcar@gmail.com>
2024-12-13 19:39:37 +0100

    Add project files.

---
+++ viska_kino/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace viska_kino.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
